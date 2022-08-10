c19fa94dd3dcb77afef82860b3b8596be3f12054
Aabid Shamji <aabidshamji@gmail.com>
2022-08-09 17:08:42 -0700

    splitter compiled

--- PaymentSplitter/contracts/PaymentSplitter.sol
+++ PaymentSplitter/contracts/PaymentSplitter.sol
@@ -2,14 +2,14 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.12;
 
-"@openzeppelin/contracts/finance/PaymentSplitter.sol";
+import "@openzeppelin/contracts/finance/PaymentSplitter.sol";
 
 contract OctiPaymentSplitter is PaymentSplitter {
    
     /**
     * @dev Contract constructor.
-    * @param royaltyAddress Address to send contract-level royalties.
-    * @param _contractURI String representing RFC 3986 URI.
+    * @param _payees List of addresses to be eligible for withdrawal
+    * @param _shares Shares assigned to each address
     */
     constructor(address[] memory _payees, uint256[] memory _shares) PaymentSplitter(_payees, _shares) payable { }
 
