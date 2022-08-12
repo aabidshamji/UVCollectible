44a6c7319abef888a586dbb760c9cbce6a6ce76f
Aabid Shamji <aabidshamji@gmail.com>
2022-08-11 17:38:17 -0700

    added networks to hardhat config

--- ERC721-with-EIP2981/scripts/deploy.js
+++ ERC721-with-EIP2981/scripts/deploy.js
@@ -3,14 +3,23 @@ const hre = require("hardhat");
 async function main() {
     const NFT = await hre.ethers.getContractFactory("Token");
     const nft = await NFT.deploy(
-        "0xd8da6bf26964af9d7eed9e03e53415d37aa96045",
+        "0x9367Ee417ae552cb94f3249d0424000747877AA8",
+        "0xdF81E19912896af20fF1be1c1Bb4487f6Ff423E0",
         "https://testnets.ultraviolet.world/metadata/polygon/"
     );
     await nft.deployed();
     console.log("NFT deployed to:", nft.address);
+
+    // This solves the bug in Mumbai network where the contract address is not the real one
+    const txHash = nft.deployTransaction.hash
+    const txReceipt = await ethers.provider.waitForTransaction(txHash)
+    const contractAddress = txReceipt.contractAddress
+    console.log("CONFIRMED: NFT deployed to", contractAddress)
 }
 
 main().then(() => process.exit(0)).catch(error => {
     console.error(error);
     process.exit(1);
 });
+
+// npx hardhat run scripts/deploy.js --network mumbai
