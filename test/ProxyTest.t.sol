82b3484429dc53c7668b49d0b6f8a0ad2a884465
Aabid Shamji <aabidshamji@gmail.com>
2022-12-23 16:56:29 -0500

    fixed proxy tests

--- test/ProxyTest.t.sol
+++ test/ProxyTest.t.sol
@@ -3,12 +3,37 @@ pragma solidity ^0.8.13;
 
 import "forge-std/Test.sol";
 import "../src/UVCollectible.sol";
-import "@openzeppelin/contracts-upgradeable/proxy/ERC1967/ERC1967UpgradeUpgradeable.sol";
+import "@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.sol";
+
+interface CheatCodes {
+    // Gets address for a given private key, (privateKey) => (address)
+    function addr(uint256) external returns (address);
+}
 
 contract ProxyTest is Test {
+    UVCollectible public logic;
+    ERC1967Proxy public proxy;
+    UVCollectible public proxied;
 
-    UVCollectible implementation = new UVCollectible();
-    
-    ERC1967UpgradeUpgradeable proxy = new ERC1967UpgradeUpgradeable();
+    address public owner;
+    address public uvadmin;
+    address public user;
 
+    CheatCodes cheats = CheatCodes(HEVM_ADDRESS);
+
+    function setUp() public {
+        owner = address(this);
+        uvadmin = cheats.addr(1);
+        user = cheats.addr(2);
+        // (1) Create logic contract
+        logic = new UVCollectible();
+
+        // (2) Create proxy and tell it which logic contract to use
+        proxy = new ERC1967Proxy(address(logic), "");
+
+        // (3) To be able to call functions from the logic contract, we need to
+        //     cast the proxy to the right type
+        proxied = UVCollectible(address(proxy));
+        proxied.initialize("UVCollectible", "", "", address(uvadmin));
+    }
 }
--- test/UVCollectible.t.sol
+++ test/UVCollectible.t.sol
@@ -2,42 +2,15 @@
 pragma solidity ^0.8.13;
 
 import "forge-std/Test.sol";
+import "./ProxyTest.t.sol";
 import "../src/UVCollectible.sol";
 
-interface CheatCodes {
-    // Gets address for a given private key, (privateKey) => (address)
-    function addr(uint256) external returns (address);
-}
-
-contract UVCollectableTest is Test {
-    UVCollectible public collectible;
-
-    address public owner;
-    address public uvadmin;
-    address public user;
-
-    CheatCodes cheats = CheatCodes(HEVM_ADDRESS);
-
-    function setUp() public {
-        owner = address(this);
-        uvadmin = cheats.addr(1);
-        user = cheats.addr(2);
-        collectible = new UVCollectible();
-        collectible.transferOwnership(owner);
-        // collectible.initialize(
-        // "UVCollectible",
-        // "UVC",
-        // "www.tokens.uv.club/creator/",
-        // uvadmin
-        // );
-    }
-
+contract UVCollectableTest is ProxyTest {
     function testName() public {
+        assertEq(proxied.name(), "UVCollectible");
         vm.startPrank(uvadmin);
-        collectible.updateName("UVCollectible");
-        assertEq(collectible.name(), "UVCollectible");
-        collectible.updateName("UVCollectibleNew");
-        assertEq(collectible.name(), "UVCollectibleNew");
+        proxied.updateName("UVCollectibleNew");
+        assertEq(proxied.name(), "UVCollectibleNew");
         vm.stopPrank();
     }
 }
